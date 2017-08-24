edd83763f313e2c0e1f4b32071d004eea4091ab1
Jordan Last <jordan.michael.last@gmail.com>
2017-08-24 16:12:46 -0600

    adding the services that we need

---
+++ services/graphql-service.ts
@@ -0,0 +1,107 @@
+import {GQLQueryDataCallback} from '../../prendus-question-elements/prendus-question-elements.d';
+import {GQLQueryErrorCallback} from '../../prendus-question-elements/prendus-question-elements.d';
+import {GQLSubscribeCallback} from '../../prendus-question-elements/prendus-question-elements.d';
+import {GQLMutateErrorCallback} from '../../prendus-question-elements/prendus-question-elements.d';
+import {getGraphcoolHTTPEndpoint, getGraphcoolWebSocketEndpoint} from './utilities-service';
+
+const httpEndpoint = getGraphcoolHTTPEndpoint();
+
+//TODO the GraphQL web socket protocol used below is deprecated and will be changing soon: https://github.com/apollographql/subscriptions-transport-ws/issues/149
+//TODO We'll need to wait for graph.cool to update their back end before we change our client
+let webSocket = new WebSocket(getGraphcoolWebSocketEndpoint(), 'graphql-subscriptions');
+let subscriptions: {
+    [key: string]: GQLSubscribeCallback
+} = {};
+
+webSocket.onopen = (event) => {
+    const message = {
+        type: 'init'
+    };
+
+    webSocket.send(JSON.stringify(message));
+};
+
+webSocket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    switch (data.type) {
+        case 'init_success': {
+            break;
+        }
+        case 'init_fail': {
+            throw {
+                message: 'init_fail returned from WebSocket server',
+                data
+            };
+        }
+        case 'subscription_data': {
+            subscriptions[data.id](data);
+            break;
+        }
+        case 'subscription_success': {
+            break;
+        }
+        case 'subscription_fail': {
+            throw {
+                message: 'subscription_fail returned from WebSocket server',
+                data
+            };
+        }
+    }
+};
+
+export const GQLRequest = async (queryString: string, variables: { [key: string]: any }, userToken: string | null, errorCallback: GQLQueryErrorCallback) => {
+
+    //TODO to allow for good cacheing, we'll probably need to parse the queryString so that we can get all of the properties that we need
+
+    const response = await window.fetch(httpEndpoint, {
+        method: 'post',
+        headers: {
+            'content-type': 'application/json',
+            ...userToken && {
+                'Authorization': `Bearer ${userToken}`
+            } //TODO As far as I understand the syntax above will be standard and this TypeScript error might go away with the following: https://github.com/Microsoft/TypeScript/issues/10727
+        },
+        body: JSON.stringify({
+            query: queryString,
+            variables
+        })
+    });
+
+    const responseJSON = await response.json();
+    const data = responseJSON.data;
+    const errors = responseJSON.errors;
+
+    (errors || []).forEach((error: any) => {
+    //   console.log('error', error)
+        errorCallback(error);
+    });
+
+    return data;
+};
+
+//TODO potentially gaurd against multiple subscriptions
+export const GQLSubscribe = (queryString: string, id: string, callback: GQLSubscribeCallback) => {
+    // we need to wait for the webSocket's connection to be open
+    if (webSocket.readyState !== webSocket.OPEN) {
+        setTimeout(() => {
+            GQLSubscribe(queryString, id, callback); // allow other tasks to run by throwing this function onto the event loop. This creates an infinite non-blocking retry
+        });
+        return;
+    }
+
+    subscriptions[id] = callback;
+
+    const message = {
+        id,
+        type: 'subscription_start',
+        query: queryString
+    };
+
+    webSocket.send(JSON.stringify(message));
+};
+
+
+export function escapeString(string: string) {
+    return string.replace(/\n/g, '\\n').replace(/"/g, '\\"');
+}
